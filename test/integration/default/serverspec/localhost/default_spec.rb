e91c91a130db065baf92e4bf00d9bffc741691a4
Kinnan Kwok <kkwoker@gmail.com>
2016-01-19 14:45:31 -0800

    Revert "Adding test for libsodium"
    This reverts commit 17b843220eec230a0548c7b1541cbe619af3472e.

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -5,8 +5,4 @@ describe 'ansible-php::default' do
   describe package('php5-fpm') do
     it { should be_installed.by('apt') }
   end
-
-  describe package('libsodium') do
-    it { should be_installed }
-  end
 end
