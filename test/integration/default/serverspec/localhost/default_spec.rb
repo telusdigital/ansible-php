17b843220eec230a0548c7b1541cbe619af3472e
Kinnan Kwok <kkwoker@gmail.com>
2016-01-19 14:22:40 -0800

    Adding test for libsodium

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -5,4 +5,8 @@ describe 'ansible-php::default' do
   describe package('php5-fpm') do
     it { should be_installed.by('apt') }
   end
+
+  describe package('libsodium') do
+    it { should be_installed }
+  end
 end
