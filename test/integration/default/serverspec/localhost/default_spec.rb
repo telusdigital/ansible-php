7aeb3331d02b52c0b79adf80658698d7498afdc3
Ben Visser <theodore.r.visser@gmail.com>
2016-01-26 13:11:58 -0800

    fixing tests

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -6,25 +6,7 @@ describe 'ansible-php::default' do
     it { should be_installed.by('apt') }
   end
 
-  describe service('php5-fpm') do
-    it { should be_running }
-  end
-
   describe command('php -i') do
     its(:stdout) { should_not match /PHP Warning/ }
-  end
-
-  describe 'PHP Config' do
-    context php_config('display_errors') do
-      its(:value) { should eq 'On' }
-    end
-
-    context php_config('max_execution_time') do
-      its(:value) { should eq 30 }
-    end
-
-    context php_config('session.cookie_secure') do
-      its(:value) { should eq 'Off' }
-    end
   end
 end
